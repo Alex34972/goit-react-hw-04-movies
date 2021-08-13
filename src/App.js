c9cb590bf9753eb2d921ea79bb27ef408f129c2e
Alex34972 <alex_hifi@ukr.net>
2021-08-13 20:48:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
-  return <div className="App">hello</div>;
-}
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Container from './component/container';
+import AppBar from './component/AppBar';
+import HomePage from './views/home';
+import MoviesPage from './views/moviesPage';
 
-export default App;
+export default function App() {
+  return (
+    <Container>
+      <AppBar />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/movies">
+          <MoviesPage />
+        </Route>
+      </Switch>
+    </Container>
+  );
+}
