a6a4438039def87ef7d07b64c69cf97e43aad2c8
Alex34972 <alex_hifi@ukr.net>
2021-08-19 14:33:17 +0300

    Update Button.js

--- src/component/buttons/Button.js
+++ src/component/buttons/Button.js
@@ -1,4 +1,8 @@
+import PropTypes from 'prop-types';
 import s from './Button.module.css';
 export default function Button({ text }) {
   return <button className={s.button}>{text}</button>;
 }
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
