3f72c56fccc61a4747a61f0cff718af97f726f63
Alex34972 <alex_hifi@ukr.net>
2021-08-20 16:08:00 +0300

    Update NotFoundView.js

--- src/views/notFoundView/NotFoundView.js
+++ src/views/notFoundView/NotFoundView.js
@@ -1,9 +1,8 @@
 import s from './NotFoundView.module.css';
-
 export default function NotFoundView() {
   return (
     <div className={s.wrapper}>
-      <h1>The page does not exist! </h1>
+      <h1>404 The page does not exist! </h1>
     </div>
   );
 }
